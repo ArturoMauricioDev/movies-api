448423006c918ed8bdf0e1117e1046a5e28b5eb1
Arturo <atercerosb@univalle.edu>
2020-10-29 10:27:00 -0400

    coneccion mongodb y metodos CRUD

---
+++ config/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const config = {
+    dev: process.env.NODE_ENV !== 'production',
+    /** cuando no esté en prodccion */
+    port: process.env.PORT || 3000,
+    /** buena practica tener una variable global para el puerto */
+    cors: process.env.CORS,
+    dbUser: process.env.DB_USER,
+    dbPassword: process.env.DB_PASSWORD,
+    dbHost: process.env.DB_HOST,
+    dbName: process.env.DB_NAME
+}
+
+module.exports = { config };
---
+++ services/movies.js
@@ -0,0 +1,33 @@
+// const { moviesMock } = require('../utils/mocks/movies')
+const MongoLib = require('../lib/mongo')
+
+class MoviesService {
+    constructor() {
+        this.collection = 'movies'
+        this.mongodb = new MongoLib()
+    }
+
+    async getMovies({ tags }) {
+        const query = tags && { tags: { $in: tags } }
+        const movies = await this.mongodb.getAll(this.collection, query)
+        return movies || []
+    }
+    async getMovie({ movieId }) {
+        const movie = await this.mongodb.get(this.collection, movieId)
+        return movie || {}
+    }
+    async createMovie({ movie }) {
+        const createdMovieId = await this.mongodb.create(this.collection, movie)
+        return createdMovieId
+    }
+    async updatedMovie({ movieId, movie } = {}) {
+        const updatedMovieId = await this.mongodb.update(this.collection, movieId, movie)
+        return updatedMovieId
+    }
+    async deletedMovie({ movieId }) {
+        const deletedMovieId = await this.mongodb.delete(this.collection, movieId)
+        return deletedMovieId
+    }
+}
+
+module.exports = MoviesService
